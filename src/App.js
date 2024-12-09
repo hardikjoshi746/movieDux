2902de91334ef84d32eeae4435918ac5a53aea2a
hardikjoshi746 <114323100+hardikjoshi746@users.noreply.github.com>
2024-12-09 14:59:42 -0500

    added the header and footer

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
